a688366accb06b1d536ba3799c7bbc728041846b
chandrikasaranya <128368735+chandrikasaranya@users.noreply.github.com>
2024-08-13 14:08:11 +0530

    Create Hero.js

---
+++ Hero.js
@@ -0,0 +1,30 @@
+// Hero.js
+export default function Hero() {
+    return (
+      <section
+        id="hero"
+        className="px-10 w-full flex gap-12 flex-col lg:flex-row 
+                   justify-center items-center align-center mt-40 
+                   mb-16 lg:mt-10 max-w-5xl mx-auto lg:gap-0 h-[80vh]">
+        <div className="flex-1 flex flex-col justify-center items-center gap-5">
+          <div>
+            <h4 className="text-center text-xl font-bold">
+                Hi and welcome !
+            </h4>
+            <h2 className="text-center text-emerald-500 text-5xl font-bold">
+              This is Saranya
+            </h2>
+          </div>
+          <p className="text-center">
+            I am a passionate student and kuchupudi dancer...
+          </p>
+        </div>
+        <div className="flex-1">
+          <img src=
+  "https://www.freepik.com/premium-ai-image/3d-cute-cartoon-girl-studying-education-illustration_64978131.htm"
+               alt="Hello.svg"
+               className="w-full h-full bg-cover"/>
+        </div>
+      </section>
+    );
+  }
