37c7af9e504068b8bc9bb8f21c55649399211345
chandrikasaranya <128368735+chandrikasaranya@users.noreply.github.com>
2024-08-13 14:10:59 +0530

    Update port1.js

--- port1.js
+++ port1.js
@@ -4,12 +4,14 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 
 export default function App() {
     return (
         <>
             <Navbar />
+            <Hero />
             <About />
             <Skills />
             <Projects />
