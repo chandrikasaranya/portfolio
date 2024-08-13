ad5a74684c874201d264b53e9b7ab00a86263311
chandrikasaranya <128368735+chandrikasaranya@users.noreply.github.com>
2024-08-13 14:26:39 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <section className="p-10 bg-emerald-200 flex justify-center">
+            <p className="font-bold">
+               Thankyou !Lets Catch Up Soon !
+            </p>
+        </section>
+    );
+}
