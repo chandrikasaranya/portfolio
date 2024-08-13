a09a28ec7e62fcc82948fee5d6c02bb17f6051c1
chandrikasaranya <128368735+chandrikasaranya@users.noreply.github.com>
2024-08-13 13:50:45 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,34 @@
+// Contact.js
+export default function Contact() {
+    return (
+        <section id="contact"
+                 className="my-40 align-center max-w-5xl mx-auto p-3">
+            <h2 className="text-5xl font-bold text-emerald-500 text-center">
+                Contact
+            </h2>
+            <div className="flex gap-5 justify-center 
+                        my-10">
+                <a rel="noreferrer"
+                   target="_blank"
+                   className="text-center hover:underline"
+                   href=
+"https://www.instagram.com/?hl=en">
+                    Instagram:
+                    <span className="font-bold">
+                        @chandrika__saranya
+                    </span>
+                </a>
+                <a rel="noreferrer"
+                   target="_blank"
+                   className="text-center hover:underline"
+                   href=
+"https://www.linkedin.com/in/pentapati-sai-sri-chandrika-saranya-710b24254">
+                    linkedin:
+                    <span className="font-bold">
+                        @PENTAPATI SAI SRI CHANDRIKA SARANYA
+                    </span>
+                </a>
+            </div>
+        </section>
+    );
+}
